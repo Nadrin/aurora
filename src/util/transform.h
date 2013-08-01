3d511efa528b37915ba3c1c00853b9b453aae3d2
Michał Siejak <michal@siejak.pl>
2013-08-02 00:00:04 +0200

    Fixed normal transformation bug.

--- src/util/transform.h
+++ src/util/transform.h
@@ -30,9 +30,9 @@ public:
 	inline float3 getNormal(const float3& n) const
 	{
 		float3 result;
-		result.x = normalMatrix[0][0] * n.x + normalMatrix[1][0] * n.y + normalMatrix[2][0] * n.z + normalMatrix[3][0];
-		result.y = normalMatrix[0][1] * n.x + normalMatrix[1][1] * n.y + normalMatrix[2][1] * n.z + normalMatrix[3][1];
-		result.z = normalMatrix[0][2] * n.x + normalMatrix[1][2] * n.y + normalMatrix[2][2] * n.z + normalMatrix[3][2];
+		result.x = normalMatrix[0][0] * n.x + normalMatrix[1][0] * n.y + normalMatrix[2][0] * n.z;
+		result.y = normalMatrix[0][1] * n.x + normalMatrix[1][1] * n.y + normalMatrix[2][1] * n.z;
+		result.z = normalMatrix[0][2] * n.x + normalMatrix[1][2] * n.y + normalMatrix[2][2] * n.z;
 		return result;
 	}
 
